a43030f1c0cce40f632b1a2be7cf133b6430cb5b
sabina-tochka <sabinabdulmanova@gmail.com>
2026-07-08 18:33:44 -0400

    push upd

--- javascript/exercises/exercise01.js
+++ javascript/exercises/exercise01.js
@@ -10,39 +10,32 @@ const exercise01 = function(p) {
 
         canvas.parent("canvas-container-1");
 
-        p.noLoop();   // Static composition
+        p.noLoop();
     };
 
 
     p.draw = function() {
 
-        // Background
         p.background(219,198,255);
 
-      // rectangle: x, y, width, height
-  fill(244, 239, 180);  // Yellow
-  noStroke();         // No border
-  rect(166, 225, 200, 200);
+        p.fill(244,239,180);
+        p.noStroke();
+        p.rect(166,225,200,200);
 
-  // rectangle: x, y, width, height
-  fill(219,198,255);  // Yellow
-  noStroke();         // No border
-  rect(180, 300, 70, 70);
+        p.fill(219,198,255);
+        p.noStroke();
+        p.rect(180,300,70,70);
 
-  // rectangle: x, y, width, height
-  fill(185, 255, 215);  // Yellow
-  noStroke();         // No border
-  rect(482, 225, 200, 200);
-  
-  // rectangle: x, y, width, height
-  fill(219,198,255);  // Yellow
-  noStroke();         // No border
-  rect(598, 300, 70, 70);
+        p.fill(185,255,215);
+        p.noStroke();
+        p.rect(482,225,200,200);
+
+        p.fill(219,198,255);
+        p.noStroke();
+        p.rect(598,300,70,70);
 
     };
 
 };
 
-
-// Create this p5 instance
 new p5(exercise01);
